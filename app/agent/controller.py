ba02889e47a8646e3af3a9f3d6840d77f7ea2afe
Nasir Mustapha <teboy.com@gmail.com>
2022-10-26 16:36:02 +0100

    POST, PUT, DEL, GET agent

--- app/agent/controller.py
+++ app/agent/controller.py
@@ -1,9 +1,51 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from app.agent.model import *
 from app.agent.schema import *
 bp = Blueprint('agent', __name__)
 
-@bp.get('/index')
-def index():
-    return 'agent Works'
+@bp.post('/agent')
+def create_agent():
+    image = request.json.get('image')
+    name = request.json.get('name')
+    name = request.json.get('name')
+    email = request.json.get('email')
+    phone = request.json.get('phone')
+    home_address = request.json.get('home_address')
+    work_address = request.json.get('work_address')
+    agent = Agent.create(image, name, email, phone, home_address, work_address)
+    return AgentSchema().dump(agent), 201
+
+@bp.get('/agent/<int:id>')
+def get_agent(id):
+    agent = Agent.get_by_id(id)
+    if agent is None:
+        return {'message': 'Agent not found'}, 404
+    return AgentSchema().dump(agent), 200
+
+@bp.patch('/agent/<int:id>')
+def update_agent(id):
+    agent = Agent.get_by_id(id)
+    if agent is None:
+        return {'message': 'Agent not found'}, 404
+    image = request.json.get('image')
+    name = request.json.get('name')
+    email = request.json.get('email')
+    phone = request.json.get('phone')
+    home_address = request.json.get('home_address')
+    work_address = request.json.get('work_address')
+    agent.update(image, name, email, phone, home_address, work_address)
+    return AgentSchema().dump(agent), 200
+
+@bp.delete('/agent/<int:id>')
+def delete_agent(id):
+    agent = Agent.get_by_id(id)
+    if agent is None:
+        return {'message': 'Agent not found'}, 404
+    agent.delete()
+    return {'message': 'Agent deleted successfully'}, 200
+
+@bp.get('/agents')
+def get_agents():
+    agents = Agent.get_all()
+    return AgentSchema(many=True).dump(agents), 200
--- app/agent/model.py
+++ app/agent/model.py
@@ -1,2 +1,48 @@
 from app import db
 
+class Agent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True)
+    phone = db.Column(db.String, unique=True)
+    home_address = db.Column(db.String, default='')
+    work_address = db.Column(db.String, default='')
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now())
+    is_deleted = db.Column(db.Boolean, default=False)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, image=None, name=None, email=None, phone=None, home_address=None, work_address=None):
+        self.image = image or self.image
+        self.name = name or self.name
+        self.email = email or self.email
+        self.phone = phone or self.phone
+        self.home_address = home_address or self.home_address
+        self.work_address = work_address or self.work_address
+        self.updated_at = db.func.now()
+        db.session.commit()
+    
+    def delete(self):
+        self.is_deleted = True
+        self.email = None
+        self.phone = None
+        self.updated_at = db.func.now()
+        db.session.commit()
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.filter_by(id=id, is_deleted=False).first()
+    
+    @classmethod
+    def get_all(cls):
+        return cls.query.filter_by(is_deleted=False).all()
+    
+    @classmethod
+    def create(cls, image, name, email, phone, home_address, work_address):
+        agent = cls(image=image, name=name, email=email, phone=phone, home_address=home_address, work_address=work_address)
+        agent.save()
+        return agent
