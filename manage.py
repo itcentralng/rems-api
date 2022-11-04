b3f40dc05fcb47b9e702d0c516cb06db2e914ac9
Nasir Mustapha <teboy.com@gmail.com>
2022-11-04 11:26:38 +0100

    perform action to delete units of deleted properties

--- manage.py
+++ manage.py
@@ -1,12 +1,10 @@
 from datetime import datetime
 from app import app
-from app.user.model import User
+from app.property.model import Property
 
 with app.app_context():
-    pass
-    # TODO:
-    # Remove the pass keyword and
-    # Run some actions you want to be be performed
-    # on your server either when deployed or locally.
-    # TODO: WARNING: This will also execute when deployed
+    properties = Property.query.filter(Property.is_deleted == True).all()
+    for property in properties:
+        for unit in property.units:
+            unit.delete()
     print('Ready!')
